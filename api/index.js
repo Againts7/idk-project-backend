5575f9f4c883827dc06d5361fa6127a34883d25e
Your Name <you@example.com>
2024-09-21 07:14:13 +0700

    On branch main Changes to be committed: 	new file:   api/index.js 	modified:   package.json 	modified:   server.js

---
+++ api/index.js
@@ -0,0 +1,10 @@
+const dotenv = require("dotenv");
+const app = require("./app");
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3030;
+
+app.listen(PORT, () => {
+  console.log(`server berjalan di: http://localhost:${PORT}`);
+});
--- server.js
+++ server.js
@@ -1,10 +1,1 @@
-const dotenv = require('dotenv');
-const app = require('./app');
 
-dotenv.config();
-
-const PORT = process.env.PORT || 3030;
-
-app.listen(PORT, () => {
-  console.log(`server berjalan di: http://localhost:${PORT}`);
-});
